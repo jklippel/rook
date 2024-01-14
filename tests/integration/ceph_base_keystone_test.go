cc84c68a05acb52f22e1d911d7f2da42fea0d2c8
Jan Klippel <jan.klippel@uhurutec.com>
2024-01-14 15:20:47 +0100

    fixup! use gateway.caBundleRef to provide a ca-bundle

--- tests/integration/ceph_base_keystone_test.go
+++ tests/integration/ceph_base_keystone_test.go
@@ -758,7 +758,7 @@ func runSwiftE2ETest(t *testing.T, helper *clients.TestClient, k8sh *utils.K8sHe
 	})
 
 	t.Run("create internal swift endpoint in keystone", func(t *testing.T) {
-		execInOpenStackClient(t, k8sh, namespace, true, "openstack", "endpoint", "create", "--region", "default", "--enable", "swift", "internal", "'http://rook-ceph-rgw-default.keystoneauth-ns.svc/swift/v1'")
+		execInOpenStackClient(t, k8sh, namespace, true, "openstack", "endpoint", "create", "--region", "default", "--enable", "swift", "internal", "http://rook-ceph-rgw-default.keystoneauth-ns.svc/swift/v1")
 	})
 
 	t.Run("create admin swift endpoint in keystone", func(t *testing.T) {
